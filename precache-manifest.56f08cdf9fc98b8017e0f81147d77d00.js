a8335e6eee74e3cf63b9f6e6ac6c588db85b05d7
Alex <CyberDasein>
2020-05-08 14:38:44 +0500

    Updates

---
+++ precache-manifest.56f08cdf9fc98b8017e0f81147d77d00.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d5b1838c3e9f592bfca69a0f0d21a9b",
+    "url": "/Game/index.html"
+  },
+  {
+    "revision": "d6eb114c94fe2200cdb4",
+    "url": "/Game/static/css/main.51c82c20.chunk.css"
+  },
+  {
+    "revision": "5f137912e70222d4cb7d",
+    "url": "/Game/static/js/2.bf96bf7f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Game/static/js/2.bf96bf7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6eb114c94fe2200cdb4",
+    "url": "/Game/static/js/main.9a52c907.chunk.js"
+  },
+  {
+    "revision": "7390fd15bc54711491e3",
+    "url": "/Game/static/js/runtime-main.529bfed6.js"
+  },
+  {
+    "revision": "0535dddbca21ce68bc9a3a5c93af4d48",
+    "url": "/Game/static/media/crosshair.0535dddb.svg"
+  }
+]);
